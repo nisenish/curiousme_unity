bad4819a5865871bf2dc11f943fcacffd490fb8f
Leeza Ricci <leezaricci@Leezas-MacBook-Air.local>
2015-11-22 23:39:48 -0800

    deleting al unnecessary stuff from repo

--- VideoTexturePlugin/src/com/sogwangtec/beijingsunyong/VideoActivity.java
+++ VideoTexturePlugin/src/com/sogwangtec/beijingsunyong/VideoActivity.java
@@ -111,6 +111,7 @@ public class VideoActivity extends UnityCardboardActivity {
 	final int				weightSum = 16;
 	final int 				MY_REQUEST_BLUETOOTH = 1002;
 	final int				max_video = 45; // number of videos are 44 right now
+	final int				max_loader = 14; // number of videos are 44 right now
 	
 	//17, 4, 6, 16 10, 11, 18, 20
 	//final int[]				videoOrder = {1, 9, 5, 15, 14, 8, 13, 2, 7, 3, 19, 12, 17, 4, 6, 16, 10, 11, 18, 20};
@@ -261,17 +262,17 @@ public class VideoActivity extends UnityCardboardActivity {
         
         // Left and Right gif views
         
-        Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay(); 
-        int catWidth = display.getWidth() / 2;
+        //Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay(); 
+        //int catWidth = display.getWidth() / 2;
         
-        
-        
+		int randomLoaderCode = r.nextInt(max_loader - 1) + 1;
+		
         waitingLeftGif = new WebView(this);
-        waitingLeftGif.loadUrl("file:///android_asset/giphy.gif");
-        waitingLeftGif.getSettings().setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);
+        waitingLeftGif.loadUrl("file:///android_asset/loaders/" + randomLoaderCode + ".gif");
+        //waitingLeftGif.getSettings().setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);
         waitingLeftGif.getSettings().setLoadWithOverviewMode(true);
         waitingLeftGif.getSettings().setUseWideViewPort(true);
-        waitingLeftGif.setPadding(0, 0, catWidth, 0);
+        //waitingLeftGif.setPadding(0, 0, catWidth, 0);
         waitingLeftGif.setBackgroundColor(Color.TRANSPARENT);
         waitingLeftGif.setLayoutParams(shakeCenterParams);
         waitingContainerLayout.addView(waitingLeftGif);
@@ -284,11 +285,11 @@ public class VideoActivity extends UnityCardboardActivity {
 		waitingLeftGif.setLayoutParams(waitingViewParams);
 		
         waitingRightGif = new WebView(this);
-        waitingRightGif.loadUrl("file:///android_asset/giphy.gif");
-        waitingRightGif.getSettings().setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);
+        waitingRightGif.loadUrl("file:///android_asset/loaders/" + randomLoaderCode + ".gif");
+        //waitingRightGif.getSettings().setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);
         waitingRightGif.getSettings().setLoadWithOverviewMode(true);
         waitingRightGif.getSettings().setUseWideViewPort(true);
-        waitingRightGif.setPadding(0, 0, catWidth, 0);
+        //waitingRightGif.setPadding(0, 0, catWidth, 0);
         waitingRightGif.setBackgroundColor(Color.TRANSPARENT);
         waitingRightGif.setLayoutParams(shakeCenterParams);
         waitingContainerLayout.addView(waitingRightGif);
